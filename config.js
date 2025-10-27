6a4950351d57255e8c5254493db257e0d1587bb1
acrilpinturascrm-svg <acrilpinturascrm@gmail.com>
2025-10-27 15:29:38 -0400

    Updates

---
+++ config.js
@@ -0,0 +1,8 @@
+// Configuración de la aplicación para GitHub Pages
+// Este archivo se puede editar directamente en la rama gh-pages sin rebuild
+window.APP_CONFIG = {
+  publicBaseUrl: 'https://acrilpinturascrm-svg.github.io/Acril-Card-control',
+  googleClientId: '647020354677-net122khnpkjv3tj13mhao4ia6dnmad.apps.googleusercontent.com',
+  googleApiKey: 'AIzaSyD_Sq3xNYYC4UNagsypZQcMQbA4QjymQds',
+  environment: 'production'
+};
